8fa36f8f7f9a30d224a87204a7b7594f0691410d
Pedro Brunet <joaopedrobrunet@hotmail.com>
2026-02-24 15:02:52 -0300

    ex01

---
+++ src/aula02/ex01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ex01 {
+    public static void main(String[] args) {
+        Scanner entradaUsuario = new Scanner(System.in);
+        String resposta;
+
+        do {
+            System.out.println("--- NOVO CADASTRO ---");
+
+            System.out.println("DIGITE SEU NOME:");
+
+            String nome = entradaUsuario.nextLine();
+
+            System.out.println("DIGITE SUA IDADE:");
+            byte idade = entradaUsuario.nextByte();
+
+            // Limpeza de buffer: consome o "Enter" que sobrou do nextByte
+            entradaUsuario.nextLine();
+
+            System.out.println("\nCadastro realizado: " + nome + ", " + idade + " anos.");
+
+
+            System.out.println("\nDeseja repetir para fazer um novo cadastro? (S/N)");
+            resposta = entradaUsuario.nextLine();
+
+        } while (resposta.equalsIgnoreCase("S"));
+
+        System.out.println("Programa encerrado. Seja bem-vindo!");
+        entradaUsuario.close();
+    }
+}
