128236319391ee9cdb709f863f079f42c7739b9d
Pedro Brunet <joaopedrobrunet@hotmail.com>
2026-03-07 14:00:18 -0300

    ex 03 calculadora

--- src/Classes/Calculadora.java
+++ src/Classes/Calculadora.java
@@ -1,4 +1,27 @@
 package Classes;
 
 public class Calculadora {
+    public double n1,n2;
+
+public Calculadora(double n1 ,double n2){
+    this.n1 = n1;
+    this.n2 = n2;}
+
+public double somar (){
+    return n1 + n2;
+}
+
+public double subtrair (){
+        return n1 - n2;
+    }
+
+
+public double mutiplicar (){
+        return n1 * n2;
+    }
+
+public double dividir(){
+    return n1 / n2 ;
+}
+
 }
--- src/aula03/ex03.java
+++ src/aula03/ex03.java
@@ -1,5 +1,52 @@
 package aula03;
+import Classes.Calculadora;
+import java.util.Scanner;
 
 public class ex03 {
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-------- CALCULADORA ---------");
+        System.out.println("insira seu nome");
+        String nome = sc.nextLine();
+
+
+        System.out.println("Seja Bem vindo a sua Calculadora " + nome);
+        System.out.println("Escolha o numero para: 1- Somar | 2- Subtrair | 3- Mutiplicar | 4- Dividir");
+        int escolha = sc.nextInt();
+        System.out.println("Digite o primeiro número:");
+        double num1 = sc.nextDouble();
+        System.out.println("Digite o segundo número:");
+        double num2 = sc.nextDouble();
+
+        Calculadora calc = new Calculadora(num1, num2);
+
+        switch (escolha) {
+            case 1:
+                System.out.println("Resultado da Soma: " + calc.somar());
+                break;
+            case 2:
+                System.out.println("Resultado da Subtração: " + calc.subtrair());
+                break;
+            case 3:
+                System.out.println("Resultado da Multiplicação: " + calc.mutiplicar());
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Resultado da Divisão: " + calc.dividir());
+                } else {
+                    System.out.println("Erro: Divisão por zero!");
+                }
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+
+
+
+
+
+
+    }
 }
