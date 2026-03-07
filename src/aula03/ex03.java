2a8cc54cb3e06c9c70d480678f2559402b534ed9
Pedro Brunet <joaopedrobrunet@hotmail.com>
2026-03-07 14:12:09 -0300

    ex 03 calculadora

--- src/aula03/ex03.java
+++ src/aula03/ex03.java
@@ -1,52 +1,54 @@
 package aula03;
-import Classes.Calculadora;
+
 import java.util.Scanner;
+import java.util.Locale;
+import Classes.Calculadora;
 
 public class ex03 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("-------- CALCULADORA ---------");
-        System.out.println("insira seu nome");
+        sc.useLocale(Locale.US);
+
+        System.out.println("Insira seu nome:");
         String nome = sc.nextLine();
+        System.out.println("Seja bem-vindo à sua Calculadora, " + nome + "!");
 
+        int opcao; // variavel pra armazenar valor dado do teclado no DO
 
-        System.out.println("Seja Bem vindo a sua Calculadora " + nome);
-        System.out.println("Escolha o numero para: 1- Somar | 2- Subtrair | 3- Mutiplicar | 4- Dividir");
-        int escolha = sc.nextInt();
-        System.out.println("Digite o primeiro número:");
-        double num1 = sc.nextDouble();
-        System.out.println("Digite o segundo número:");
-        double num2 = sc.nextDouble();
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1- Somar | 2- Subtrair | 3- Multiplicar | 4- Dividir | 0- Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = sc.nextInt();
 
-        Calculadora calc = new Calculadora(num1, num2);
+            if (opcao >= 1 && opcao <= 4) {
+                System.out.print("Digite o primeiro número: ");
+                double n1 = sc.nextDouble();
+                System.out.print("Digite o segundo número: ");
+                double n2 = sc.nextDouble();
 
-        switch (escolha) {
-            case 1:
-                System.out.println("Resultado da Soma: " + calc.somar());
-                break;
-            case 2:
-                System.out.println("Resultado da Subtração: " + calc.subtrair());
-                break;
-            case 3:
-                System.out.println("Resultado da Multiplicação: " + calc.mutiplicar());
-                break;
-            case 4:
-                if (num2 != 0) {
-                    System.out.println("Resultado da Divisão: " + calc.dividir());
-                } else {
-                    System.out.println("Erro: Divisão por zero!");
+                // Criando o objeto com os números lidos
+                Calculadora calc = new Calculadora(n1, n2);
+
+                switch (opcao) {
+                    case 1 -> System.out.println("Resultado da Soma: " + calc.somar());
+                    case 2 -> System.out.println("Resultado da Subtração: " + calc.subtrair());
+                    case 3 -> System.out.println("Resultado da Multiplicação: " + calc.mutiplicar());
+                    case 4 -> {
+                        if (n2 != 0) {
+                            System.out.println("Resultado da Divisão: " + calc.dividir());
+                        } else {
+                            System.out.println("Erro: Não é possível dividir por zero!");
+                        }
+                    }
                 }
-                break;
-            default:
+            } else if (opcao != 0) {
                 System.out.println("Opção inválida!");
-        }
+            }
 
+        } while (opcao != 0); // Continua rodando até o usuário digitar 0
 
-
-
-
-
-
+        System.out.println("Programa encerrado. Até mais, " + nome + "!");
+        sc.close();
     }
 }
